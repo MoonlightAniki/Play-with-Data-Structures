012e7441e246b0832188f97d81943126f95d1d4f
guoliang <guoliang0511@qq.com>
2018-06-18 21:30:18 +0800

    循环队列

--- 03-Stacks-and-Queues/ArrayStack/src/Main.java
+++ 03-Stacks-and-Queues/ArrayStack/src/Main.java
@@ -2,6 +2,33 @@ public class Main {
     public static void main(String[] args) {
 //        demo1();
 
+//        demo2();
+
+        int count = 100000;
+        Queue<Integer> q1 = new ArrayQueue<>();
+        double time1 = testQueue(q1, count);
+        System.out.println("ArrayQueue, count = " + count + ", time:" + time1 + " s.");
+
+
+        Queue<Integer> q2 = new LoopQueue<>();
+        double time2 = testQueue(q2, count);
+        System.out.println("LoopQueue, count = " + count + ", time:" + time2 + " s.");
+
+    }
+
+    private static double testQueue(Queue<Integer> queue, int count) {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < count; ++i) {
+            queue.enqueue(i);
+        }
+        for (int i = 0; i < count; ++i) {
+            queue.dequeue();
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    private static void demo2() {
         Queue<Integer> queue = new ArrayQueue<>();
         for (int i = 0; i < 17; ++i) {
             queue.enqueue(i);
