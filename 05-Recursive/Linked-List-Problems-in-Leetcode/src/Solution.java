69f2c7ebc7963594f01d4d6c2e1d396d9851a150
guoliang <guoliang@hrtpayment.com>
2018-06-20 11:33:52 +0800

    调试递归代码

---
+++ 05-Recursive/Linked-List-Problems-in-Leetcode/src/Solution.java
@@ -0,0 +1,53 @@
+// 调试递归代码
+public class Solution {
+    public ListNode removeElements(ListNode head, int val, int depth) {
+        String depthString = generateDepthString(depth);
+        System.out.print(depthString);
+        System.out.println("Call: remove " + val + " in " + head);
+        if (head == null) {
+            System.out.print(depthString);
+            System.out.println("Return: " + head);
+            return null;
+        }
+
+        ListNode res = removeElements(head.next, val, depth + 1);
+        System.out.print(depthString);
+        System.out.println("After remove " + val + ": " + res);
+
+        ListNode ret;
+        if (head.val == val) {
+            ret = res;
+        } else {
+            head.next = res;
+            ret = head;
+        }
+
+        System.out.print(depthString);
+        System.out.println("Return: " + ret);
+
+        return ret;
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; ++i) {
+            res.append("--");
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 6, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        Solution solution = new Solution();
+        head = solution.removeElements(head, 6, 0);
+        System.out.println(head);
+
+//        nums = new int[]{6, 6, 6, 1, 2, 3, 6, 6, 4, 5, 6, 6};
+//        head = new ListNode(nums);
+//        head = solution.removeElements(head, 6, 0);
+//        System.out.println(head);
+    }
+}
